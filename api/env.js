cdac54eede85406e35bc52f5a35c2abf830ea69a
Miguel Levi <81972447+miguellevi@users.noreply.github.com>
2026-04-18 15:29:29 -0300

    Delete api directory

--- api/env.js
+++
@@ -1,7 +0,0 @@
-module.exports = function handler(req, res) {
-  res.setHeader('Content-Type', 'application/javascript');
-  res.send(`
-    window.__SGA_SUPABASE_URL__  = "${process.env.SUPABASE_URL || ''}";
-    window.__SGA_SUPABASE_ANON__ = "${process.env.SUPABASE_ANON_KEY || ''}";
-  `);
-};
